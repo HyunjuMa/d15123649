afac385cb8b1712678259b78c4c75547fa4fc377
mhj <mahyunju@mahyunju.local>
2015-09-23 16:54:21 +0100

    prob1 completed

---
+++ lab2_prob1/lab2_prob1/prob1.cpp
@@ -0,0 +1,29 @@
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    
+    long long int sum=0;
+    int x = (floor(1000/3));
+    int y = (floor(1000/5));
+    int z = (floor(1000/15));
+    
+    for (int i=1; i<=x; i++) {
+        sum+=i*3;
+    }
+    
+    for (int j=1; j<=y; j++) {
+        sum+=j*5;
+    }
+    
+    for (int k=1; k<=z; k++) {
+        sum-=k*15;
+    }
+    
+    cout << sum << endl;
+    
+    return 0;
+}
